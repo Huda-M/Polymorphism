21f38755ceaa3436c69d329c7fa17e0aa3d83100
Huda-M <hodaelawady@std.mans.edu.eg>
2025-05-08 00:03:33 +0300

    Add files via upload

---
+++ Polymorphism/task1.php
@@ -0,0 +1,66 @@
+<?php 
+class Product
+{
+    private $name;
+    public $price;
+    public $brand;
+    public $image;
+    public $description;
+    public $tax;
+
+    public function __construct($name, $price, $brand, $image, $description, $tax)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->brand = $brand;
+        $this->image = $image;
+        $this->description = $description;
+        $this->tax = $tax;
+    }
+
+    public function get_name()
+    {
+        return $this->name;
+    }
+
+    public function Price_after_discound(float $discound)
+    {
+        if ($discound > 0 && $discound < 1) {
+            return $this->price * (1 - $discound);
+        } else {
+            return $this->price;
+        }
+    }
+
+    public function Get_finall_price($discound = 0)
+    {
+        $price_after_discount = $this->Price_after_discound($discound);
+
+        if ($this->tax > 0) {
+            return $price_after_discount + ($price_after_discount * $this->tax);
+        } else {
+            return $price_after_discount;
+        }
+    }
+}
+$product=new product( "phone",5000,"samsung","image of first product","first product",.1);
+echo $product->get_name();
+echo "<br>";
+echo $product->Price_after_discound(.1);
+echo "<br>";
+echo $product->Get_finall_price();
+echo "<br><br>";
+$product=new product( "lab",50000,"samsung","image of secound product","secound product",.5);
+echo $product->get_name();
+echo "<br>";
+echo $product->Price_after_discound(.3);
+echo "<br>";
+echo $product->Get_finall_price();
+echo "<br><br>";
+$product=new product( "tab",15000,"samsung","image of third product","secound product",.3);
+echo $product->get_name();
+echo "<br>";
+echo $product->Price_after_discound(.2);
+echo "<br>";
+echo $product->Get_finall_price();
+echo "<br><br>";
